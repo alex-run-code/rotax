211d7b097e3f117204f4de2f6755caa4b6fe870b
unknown <megalex97@hotmail.com>
2020-06-20 17:27:46 +0300

    now handle error if the user input nothing or if nothing is found

--- rotax/taxreader/readtax.py
+++ rotax/taxreader/readtax.py
@@ -4,35 +4,42 @@ from taxreader.models import CompanyTaxInfo
 def get_tax_info(cui, date):
     post = [{"cui": cui, "data":date }]
     re = requests.post('https://webservicesp.anaf.ro/PlatitorTvaRest/api/v4/ws/tva', json=post)
-    json_response = re.json()
-    cui = json_response['found'][0]['cui']
-    new_company = CompanyTaxInfo(
-        cui=json_response['found'][0]['cui'],
-        data=json_response['found'][0]['data'],
-        denumire=json_response['found'][0]['denumire'],
-        adresa=json_response['found'][0]['adresa'],
-        data_inceput_ScpTVA=json_response['found'][0]['data_inceput_ScpTVA'],
-        data_sfarsit_ScpTVA=json_response['found'][0]['data_sfarsit_ScpTVA'],
-        data_anul_imp_ScpTVA=json_response['found'][0]['data_anul_imp_ScpTVA'],
-        mesaj_ScpTVA=json_response['found'][0]['mesaj_ScpTVA'],
-        dataInceputTvaInc=json_response['found'][0]['dataInceputTvaInc'],
-        dataSfarsitTvaInc=json_response['found'][0]['dataSfarsitTvaInc'],
-        dataActualizareTvaInc=json_response['found'][0]['dataActualizareTvaInc'],
-        dataPublicareTvaInc=json_response['found'][0]['dataPublicareTvaInc'],
-        tipActTvaInc=json_response['found'][0]['tipActTvaInc'],
-        statusTvaIncasare=json_response['found'][0]['statusTvaIncasare'],
-        dataInactivare=json_response['found'][0]['dataInactivare'],
-        dataReactivare=json_response['found'][0]['dataReactivare'],
-        dataPublicare=json_response['found'][0]['dataPublicare'],
-        dataRadiere=json_response['found'][0]['dataRadiere'],
-        statusInactivi=json_response['found'][0]['statusInactivi'],
-        dataInceputSplitTVA=json_response['found'][0]['dataInceputSplitTVA'],
-        dataAnulareSplitTVA=json_response['found'][0]['dataAnulareSplitTVA'],
-        statusSplitTVA=json_response['found'][0]['statusSplitTVA'],
-        iban=json_response['found'][0]['iban'],
-    )
-    new_company.save()
-    print(json_response['found'][0]['denumire'], ' - added')
+    if re.status_code == 200:
+        json_response = re.json()
+        print(json_response['found'][0]['denumire'])
+        print(len(json_response['found'][0]['denumire']))
+        if len(json_response['found'][0]['denumire']) != 0:
+            cui = json_response['found'][0]['cui']
+            new_company = CompanyTaxInfo(
+                cui=json_response['found'][0]['cui'],
+                data=json_response['found'][0]['data'],
+                denumire=json_response['found'][0]['denumire'],
+                adresa=json_response['found'][0]['adresa'],
+                data_inceput_ScpTVA=json_response['found'][0]['data_inceput_ScpTVA'],
+                data_sfarsit_ScpTVA=json_response['found'][0]['data_sfarsit_ScpTVA'],
+                data_anul_imp_ScpTVA=json_response['found'][0]['data_anul_imp_ScpTVA'],
+                mesaj_ScpTVA=json_response['found'][0]['mesaj_ScpTVA'],
+                dataInceputTvaInc=json_response['found'][0]['dataInceputTvaInc'],
+                dataSfarsitTvaInc=json_response['found'][0]['dataSfarsitTvaInc'],
+                dataActualizareTvaInc=json_response['found'][0]['dataActualizareTvaInc'],
+                dataPublicareTvaInc=json_response['found'][0]['dataPublicareTvaInc'],
+                tipActTvaInc=json_response['found'][0]['tipActTvaInc'],
+                statusTvaIncasare=json_response['found'][0]['statusTvaIncasare'],
+                dataInactivare=json_response['found'][0]['dataInactivare'],
+                dataReactivare=json_response['found'][0]['dataReactivare'],
+                dataPublicare=json_response['found'][0]['dataPublicare'],
+                dataRadiere=json_response['found'][0]['dataRadiere'],
+                statusInactivi=json_response['found'][0]['statusInactivi'],
+                dataInceputSplitTVA=json_response['found'][0]['dataInceputSplitTVA'],
+                dataAnulareSplitTVA=json_response['found'][0]['dataAnulareSplitTVA'],
+                statusSplitTVA=json_response['found'][0]['statusSplitTVA'],
+                iban=json_response['found'][0]['iban'],
+            )
+            new_company.save()
+            print(json_response['found'][0]['denumire'], ' - added')
+    else:
+        return(False)
+
 
 
 #  response example:
--- rotax/taxreader/views.py
+++ rotax/taxreader/views.py
@@ -4,6 +4,7 @@ from taxreader.models import CompanyTaxInfo
 from django.http import JsonResponse, HttpResponse
 from django.core import serializers
 import json
+from django.db.utils import IntegrityError
 
 
 # Create your views here.
@@ -15,10 +16,23 @@ def index(request):
 def get_tax_info(request):
     cui = request.GET['cui']
     date = request.GET['date']
-    readtax.get_tax_info(cui,date)
-    company = CompanyTaxInfo.objects.filter(cui=cui)
-    json_company = serializers.serialize('json', company)
-    print(json_company)
-    return HttpResponse(json_company, content_type="text/json-comment-filtered")
+    try:
+        readtax.get_tax_info(cui,date)
+    except(IntegrityError):
+        print('IntegrityError')
+    try:
+        company = CompanyTaxInfo.objects.filter(cui=cui)
+        json_company = serializers.serialize('json', company)
+        print(company)
+        print(len(company))
+        if len(company) != 0:
+            return HttpResponse(json_company, content_type="text/json-comment-filtered")
+        else:
+            return HttpResponse(False)
+    except(ValueError):
+        print('Value Error')
+        return HttpResponse(False)
+    
+
 
     
