d158a5f8ddf26cf7f18b2d143ac5184ec3e03047
unknown <megalex97@hotmail.com>
2020-06-20 19:51:50 +0300

    added a procfile for local testing

--- taxreader/urls.py
+++ taxreader/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('get_tax_info', views.get_tax_info, name='get_tax_info'),
-    path('companies', views.companies, name='companies'),
+    path('get_tax_info/', views.get_tax_info, name='get_tax_info'),
+    path('companies/', views.companies, name='companies'),
 ]
